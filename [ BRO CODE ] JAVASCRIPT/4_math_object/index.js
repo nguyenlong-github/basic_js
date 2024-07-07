51be3548fb5d3ed8c3e3c3cc0deb08c082f43383
Hai Long <nguyenhailong.works@gmail.com>
2024-07-07 12:44:13 +0900

    LESSON 4 MATH OBJECT

---
+++ [ BRO CODE ] JAVASCRIPT/4_math_object/index.js
@@ -0,0 +1,28 @@
+// Math = built-in object that provides a collection of properties and methods
+
+/*
+    Ví Dụ:
+    Thay vì phải nhâp tay giá trị của pi là:
+    let pi = 3.1415926;
+    thì có thể dùng object Math
+    cosole.log(Math.PI);
+*/
+let x = 4;
+let y = 21;
+let z;
+
+//z = Math.round(x); // làm tròn như bình thường: 3.49 -> 3 và 3.51 -> 4
+//z = Math.floor(x); // luôn làm tròn xuống 3.99 -> 3
+//z = Math.ceil(x); // ngược với floor
+//z = Math.trunc(x); // loại bỏ thập phân 3.15 -> 3
+//z = Math.pow(x,y); // lũy thừ x mũ y
+//z = Math.sqrt(x); // căn bậc 2
+//z = Math.log(x); //logarithm
+//z = Math.sin(x);
+//z = Math.abs(x); // trị tuyệt đối
+//z = Math.sign(x);//trả về dấu của một số x. 1 là dương, -1 là âm , 0 là zero
+//let max = Math.max(x,y) // Lấy ra số lớn nhất
+//let min = Math.min(x,y) // Lấy ra số nhỏ nhất
+
+console.log(max);
+
